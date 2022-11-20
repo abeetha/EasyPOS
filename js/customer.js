56113943f7620d5713364b4ca6ca14fd0ca4d511
Abeetha <abeethaabeysundera@gmail.com>
2022-11-20 15:44:18 +0530

    5

---
+++ js/customer.js
@@ -0,0 +1,94 @@
+let customers=[];
+
+function Customer(id,name,address,salary){
+    this.id=id;
+    this.name=name;
+    this.address=address;
+    this.salary=salary;
+};
+initializeCustomers=()=>{
+    let tempData = JSON.parse(localStorage.getItem('customers'));
+
+    if(tempData!==null){
+        customers=tempData;
+        console.log(tempData);
+        setTableData();
+    }
+}
+function setTableData() {
+    let htmlData='';
+    customers.forEach(data =>{
+        htmlData +=`<tr>
+            <td>${data.id}</td> 
+            <td>${data.name}</td>
+             <td>${data.address}</td> 
+             <td>${data.salary}</td>
+             <td>
+             <button class="btn btn-success btn-sm" onclick="loadUpdateModal('${data.id}','${data.name}','${data.address}', ${data.salary})">Update</button> |
+             <button class="btn btn-danger btn-sm" onclick="deleteCustomer('${data.id}')">Delete</button>
+</td>
+</tr>`;
+    });
+
+    $('#table_body').html(htmlData);
+}
+function deleteCustomer(id){
+    if(confirm('are you sure')){
+        for(let tempId=0; tempId<customers.length; tempId++){
+            if(customers[tempId].id===id){
+                customers.splice(tempId,1);
+                localStorage.setItem('customers',JSON.stringify(customers));
+                launchModal('Deleted!','Customer Deleted');
+                setTableData();
+            }
+        }
+    }
+}
+
+
+
+function saveCustomer(){
+    let customer = new Customer(
+        $('#customerId').val(),
+        $('#customerName').val(),
+        $('#customerAddress').val(),
+        Number($('#customerSalary').val())
+    );
+
+    if(customers.find(data=>customer.id==data.id)==undefined){
+    customers.push(customer);
+    localStorage.setItem('customers',JSON.stringify(customers));
+    clearFields();
+    launchModal('success!','Customer Saved');
+    setTableData();
+}else{
+        launchModal('warning!','Already Exists');
+    }
+}
+
+ const launchModal=(type, message)=>{
+    $('#exampleModalLabel').html(type);
+    $('.save-data-body').html(message);
+   /* let showMessage=message;
+    title='';
+    if(type==='success'){
+        title='Success!';
+    }else if(type==='warning'){
+        title='Warning!';
+    }*/
+    $('#success-modal').click();
+}
+
+ const clearFields = () => {
+        $('#customerId').val('');
+        $('#customerName').val('');
+        $('#customerAddress').val('');
+        $('#customerSalary').val('');
+}
+function loadUpdateModal(id,name,address,salary){
+        $('#update_customer_id').val(id);
+       $('#update_customer_name').val(name);
+        $('#update_customer_address').val(address);
+        $('#update_customer_salary').val(salary);
+        $('#update-modal-button').click();
+}
---
+++ js/dashboard.js
@@ -0,0 +1,11 @@
+loadUserDetails=()=>{
+    try{
+        let userDetails =
+            JSON.parse(localStorage.getItem('user'));
+        $('.user-name').html(userDetails.name);
+        $('#avatar').attr('src',userDetails.avatar);
+    }catch (e){
+        alert('Something went wrong!');
+        window.location.replace('../index.html');
+    }
+}
